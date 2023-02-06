e61f9cceda52fd354e7a9223c148e68aa08bf999
zweix <1979803044@qq.com>
2023-02-06 18:16:47 +0800

    modify

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/zweix.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zweix.omp.json" | Invoke-Expression
 $env:VIRTUAL_ENV_DISABLE_PROMPT=1
