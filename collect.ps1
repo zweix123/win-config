c8ae148d4a67aa930a2e7c5ca2a251a08104ca23
zweix123 <1979803044@qq.com>
2023-02-27 10:23:18 +0800

    modify terminual init condition

--- collect.ps1
+++ collect.ps1
@@ -4,4 +4,4 @@ Copy-Item $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 ./ -Force
 
 # 收集WindowsTerminal配置
 Copy-Item $HOME\'AppData\Local\Microsoft\Windows Terminal\settings.json' ./ -Force
-Copy-Item $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json ./ -Force
+# Copy-Item $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json ./ -Force
